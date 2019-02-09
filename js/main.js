e954d6a3afce2e43c7e18843c1cf24b389f1dcbc
Drew <drewboy879@gmail.com>
2019-02-09 14:24:14 -0600

    deploy

---
+++ js/main.js
@@ -0,0 +1,20 @@
+count = 1;
+function add() {
+    var countValue = document.getElementById('countValue');
+    var a = countValue.value;
+    var counter = document.getElementById("counter").textContent;
+    var c = Number(counter);
+    var total = Number(a) + Number(c);
+    document.getElementById("counter").innerHTML = total;
+
+
+}
+
+function subtract() {
+    var countValue = document.getElementById('countValue');
+    var a = countValue.value;
+    var counter = document.getElementById("counter").textContent;
+    var c = Number(counter);
+    var total = Number(c) - Number(a);
+    document.getElementById("counter").innerHTML = total;
+}
